3f90aa2ccb5f41dc2a71e35ea7f6cc3b4157086d
Hanif Carroll <hanifcarroll@gmail.com>
2025-09-02 20:27:39 -0300

    Add Clarity tracking script to the head of the RootLayout component for enhanced user analytics.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script type="text/javascript">
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "t4p7xwzuf0");
+        </script>
+      </head>
       <body
         className={`${playfair.variable} ${inter.variable} antialiased`}
       >
